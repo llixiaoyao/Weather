b679f98023ea5e3123c4429b7560754808eb07c8
WangJiacong <85786508@qq.com>
2021-08-06 14:29:51 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/coolweather/android/db/County.java
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -1,8 +1,9 @@
 package com.coolweather.android.db;
 
-import org.litepal.crud.LitePalSupport;
 
-public class County extends LitePalSupport {
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
     private int id;
     private String countyName;
     private String weatherId;
